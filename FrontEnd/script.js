a3ee7c1c52ba826f5481dfc2ef84ea75160f1a7c
lookingfrag <jessesimanjuntaks@gmail.com>
2023-10-15 18:59:39 +0700

    menyelesaikan website kpp bms yeyy untuk desktop

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,34 +1,34 @@
-/*
-// Register ScrollTrigger 
 gsap.registerPlugin(ScrollTrigger);
 
-//GSAP timeline
-const tl = gsap.timeline({
-  ease: "none"
+gsap.timeline({
+  scrollTrigger: {
+    trigger: ".part1",
+    start: "top center",
+    end: "bottom bottom",
+    scrub: true,
+  }
+})
+.to(".imageinter", {
+  x: -800, // Move the image to the left
+  y: 650, // Move the image down
+  scale: 0.7, // Scale the image down to 50% of its original size
+}, 0);
+
+//receive email
+document.getElementById("emailForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  var emailInput = document.getElementById("email");
+  var email = emailInput.value;
+
+  // Simple email validation using a regular expression
+  var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  if (!emailRegex.test(email)) {
+      alert("Please enter a valid email address.");
+      emailInput.focus();
+  } else {
+      // You can replace this alert with your own submission logic (e.g., sending the email to a server).
+      alert("Email submitted: " + email);
+      emailInput.value = ""; // Clear the input field
+  }
 });
 
-//animation
-tl.fromTo("#image", {
-  clipPath: "polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%)",
-  x: "0%",
-}, {
-  clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)", // Remove polygon
-  scale: 2, 
-  duration: 1,
-  transformOrigin: "center center",
-  x: "-50%", // gerak ke kiri
-}).to("#image", {
-  y: "50%", // Center vertical
-});
-
-// Create ScrollTrigger
-ScrollTrigger.create({
-  trigger: "#image", 
-  start: "top top", // when the animation starts (element top reaches the top of the viewport)
-  end: "+=50", // when the animation ends (element moves 200% of its height)
-  pin: true, // pin the element during the animation
-  animation: tl, // use the timeline created above
-  scrub: 0.78, // smooth scrolling effect
-  pinSpacing: false // don't maintain spacing while pinned
-});
-*/
